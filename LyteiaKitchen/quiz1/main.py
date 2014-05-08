f198d64b418af564c1c51abb196432f86eef6a08
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-07 21:44:07 -0400

    area

--- LyteiaKitchen/quiz1/main.py
+++ LyteiaKitchen/quiz1/main.py
@@ -3,3 +3,13 @@ Lyteia Kitchen
 5/7/14
 Quiz1
 '''
+
+def calcArea(w, h):
+    #area = h times width
+    area = h * w
+    #this returns the area
+    return area
+height = 40
+width = 20
+area = calcArea(height, width)
+
