af80d8ede5364c2656ebb7f45d0154202495b38a
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-07 21:44:47 -0400

    If height is same as width

--- LyteiaKitchen/quiz1/main.py
+++ LyteiaKitchen/quiz1/main.py
@@ -12,4 +12,11 @@ def calcArea(w, h):
 height = 40
 width = 20
 area = calcArea(height, width)
+    #if height is the same a width
+if height == width:
+        print "The area of your square is " + str(area)+ " square feet."
+else:
+        print "The area of your rectangle is " + str(area)+ " square feet."
 
+
+area = calcArea(height,width)
