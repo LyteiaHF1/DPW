7e07c39c250d59b4b8d607871a5e0ccc477a9084
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-07 20:17:20 -0400

    Setup in PyCharm Lab 2

---
+++ LyteiaKitchen/simpleform/main.py
@@ -0,0 +1,15 @@
+'''
+Lyteia Kitchen
+5/7/2014
+Simple Form
+Lab 2
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
