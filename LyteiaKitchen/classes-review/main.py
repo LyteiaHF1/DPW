b22b2d1b07b93a660b4d2c61676f495322bd99a3
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-12 18:09:45 -0400

    Lecture

---
+++ LyteiaKitchen/classes-review/main.py
@@ -0,0 +1,13 @@
+'''
+Lyteia K
+Lecture day 3 Review Classes
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
