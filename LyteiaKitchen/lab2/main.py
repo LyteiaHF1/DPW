5b48d218b0ff52841d29ad6dc1ac6236f00dd2c3
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-12 13:01:30 -0400

    setting up form class and html elements

--- LyteiaKitchen/lab2/main.py
+++ LyteiaKitchen/lab2/main.py
@@ -5,11 +5,13 @@ Simple Form
 Lab 2
 '''
 import webapp2
+from page import Form
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+	def get(self):
+		if self.request.GET:
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/', MainHandler)
 ], debug=True)
---
+++ LyteiaKitchen/lab2/page.py
@@ -0,0 +1,32 @@
+class Form():
+	# Init function that will run when the page is opened
+	def __init__(self, main_self):
+		#a head variable to contain the html head code
+		self.head = ''' <!DOCTYPE HTML>
+		<html>
+			<head>
+				<title>Phone app</title>
+				<link rel='stylesheet' type='text/css' href='css/main.css'/>
+			</head>
+		<body><div class='container'> '''
+		# Creating a form variable to contain the html form code
+		self.form = '''
+		<form method='GET'>
+				<h1>Phone Book</h1>
+				<label for="first_name">First Name</label>
+				<input type='text' name='first_name' id='first_name'/>
+				<label for="last_name">Last Name</label>
+				<input type='text' name='last_name' id='last_name'/>
+				<label for="phone_number">Phone Number</label>
+				<input type='text' name='phone_number' placeholder='(555) 555-5555' id='phone_number'/>
+				<p class='label'>Select Phone Type</p>
+				<select name='phone_type'>
+					<option value='phoneType'>Home</option>
+			  		<option value='Cell' selected='selected'>Cell</option>
+			  		<option value='Work'>Work</option>
+				</select>
+				<p class='label'>Relationship</p>
+				<input type='checkbox' name='relationship1' value='Friend'/><p>Friend</p>
+				<input type='checkbox' name='relationship2' value='Co-worker'/><p>Co-worker</p> <br>
+				<input type='submit' value ='Enter' id='button'/>
+		
