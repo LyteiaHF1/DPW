430f405f1424aa8797314530e745f10be54ed5f4
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-12 17:33:55 -0400

    start of page html

--- LyteiaKitchen/Encapsulated-Calculator/page.py
+++ LyteiaKitchen/Encapsulated-Calculator/page.py
@@ -1,4 +1,16 @@
 class Page():
     def __init__(self):
         self.__header = '''
-                        '''
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Lab 3 Calculator</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <h1></h1>
+            </header>
+            <div id="main">
+        '''
