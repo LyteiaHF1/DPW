cf5d86dcbed10a62a84d000bfa01b266afc4869e
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-14 17:11:15 -0400

    Lab 4 Setup

---
+++ LyteiaKitchen/fox/main.py
@@ -0,0 +1,14 @@
+'''
+Lyteia Kitchen
+Lab 4
+5/14/14
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
