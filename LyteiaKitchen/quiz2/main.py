20a252b66bb34088323392226cc667193ce8166c
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-19 23:11:35 -0400

    sub class 1

--- LyteiaKitchen/quiz2/main.py
+++ LyteiaKitchen/quiz2/main.py
@@ -10,3 +10,13 @@ class Sneaker(object):
         self._type = 'Balenciga'
         self.__debut = '2014'
         self.__color = 'White'
+
+#Sub Class
+class JeremyS(Sneaker):
+    def __init__(self):
+        super(JeremyS, self).__init__()
+
+        self.__type = 'Denim Wings'
+        self._debut = '2011'
+        self.__style = 'winged'
+        self.condition = 'Deadstock'
