da73b24e1fa75e1b776e30433705413cb4c82b7a
Lyteia Kitchen <lytehighfashion@gmail.com>
2014-05-19 23:07:24 -0400

    Sneaker Class

--- LyteiaKitchen/quiz2/main.py
+++ LyteiaKitchen/quiz2/main.py
@@ -3,3 +3,10 @@ Lyteia Kitchen
 Inheritance Quiz
 May 19,2013
 '''
+
+#Abstract Class Sneaker
+class Sneaker(object):
+    def __init__(self):
+        self._type = 'Balenciga'
+        self.__debut = '2014'
+        self.__color = 'White'
